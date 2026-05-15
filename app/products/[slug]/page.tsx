6012ba19c5edc17c476c0b60af24f372915ce092
MEISINDO <ezra.alannathaniel19@gmail.com>
2026-05-15 14:38:30 +0800

    update

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -38,7 +38,7 @@ const wineData: Record<
       },
     ],
   },
-/*
+
   "south-africa": {
   title: "South African Wine Collection",
   description:
@@ -64,7 +64,6 @@ const wineData: Record<
     },
   ],
 },
-*/
 
 "new-zealand": {
   title: "New Zealand Wine Collection",
@@ -137,7 +136,7 @@ const wineData: Record<
       },
     ],
   },
-/*
+
   spain: {
     title: "Spanish Wine Collection",
     description:
@@ -163,7 +162,6 @@ const wineData: Record<
       },
     ],
   },
-  */
 };
 
 export default async function ProductPage({
