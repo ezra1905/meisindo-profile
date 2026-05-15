701ba84aea54a5c382f5a15b4be685d5648963a3
MEISINDO <ezra.alannathaniel19@gmail.com>
2026-05-15 16:07:57 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -239,7 +239,7 @@ export default function Home() {
         <div className="absolute inset-0 bg-black/20" />
         <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-black/10 to-black/40" />
 
-        <div className="absolute inset-0 bg-black/40" />
+        <div className="absolute inset-0 bg-black/20" />
 
         <motion.div
           initial={{ opacity: 0, y: 70 }}
